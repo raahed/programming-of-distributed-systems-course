278aa5a3ab8c5a7aa1fe76733a18009a9c15baf5
Salazifel <hannes@rmgb.de>
2023-11-28 09:18:12 +0100

    server proxy done

--- task-3/server-proxy/server.py
+++ task-3/server-proxy/server.py
@@ -1,23 +1,101 @@
+import os
+from jsonrpcserver import method, Success, serve
+import threading
 import requests
-import socket as sk
+import json
 
-server_list = [
-    'https://oru-pds-serv-prim.raah.me',
-    'https://oru-pds-serv-r1.raah.me',
-    'https://oru-pds-serv-r2.raah.me'
-]
+url_primary = "https://oru-pds-serv-prim.raah.me"
+url_rep1 = "https://oru-pds-serv-r1.raah.me/"
+url_rep2 = "https://oru-pds-serv-r2.raah.me/"
 
-# Alias for http://oru-pds-serv.raah.me
-host = ('localhost', 3999)
+# client side
 
-socket = sk.socket()
+@method
+def json_rpc_request(method, params, url):
+    payload = {
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": params,
+        "id": 1,
+    }
+    response = requests.post(url, json=payload).json()
+    
+    # Check for errors in the response
+    if "error" in response:
+        print("Error:", response["error"])
+        return None
 
-socket.bind(host)
+    return response["result"]
 
-while True:
-    payload, client = socket.recvfrom(65536)
+@method
+def read_to_server(url):
+    result = json_rpc_request("read", {}, url)
+    if result is not None:
+        print("Read result:", result)
 
-    for i, server in enumerate(server_list):
+@method
+def write_to_server(data, url):
+    result = json_rpc_request("write", {"data": data}, url)
+    if result is not None:
+        print("Write result:", result)
+
+
+# server side
+
+# Define the file path
+file_path = "server_file.txt"
+
+# Lock for synchronization
+
+temp = "HAHA"; 
+
+def read():
+    try:
+        with open(file_path, 'r') as file:
+            try:
+                try:
+                    read_to_server(url_primary)
+                except Exception as e:
+                    return {"Main server error:": str(e)}
+                try:
+                    read_to_server(url_rep1)
+                except Exception as e:
+                    return {"Rep1 server error:": str(e)}
+                try:
+                    read_to_server(url_rep2)
+                except Exception as e:
+                    return {"Rep2 server error:": str(e)}
+            except Exception as e:
+                return {"All server error:" : str(e)}
+            
+            return Success("Read successful")
+    except Exception as e:
+        return {"error": str(e)}
+
+@method
+def write(data):
+    try:
         try:
-            requests.request()
+            try:
+                write_to_server(data, url_primary)
+            except Exception as e:
+                return {"Main server error:": str(e)}
+            try:
+                write_to_server(data, url_rep1)
+            except Exception as e:
+                return {"Rep1 server error:": str(e)}
+            try:
+                write_to_server(data, url_rep2)
+            except Exception as e:
+                return {"Rep2 server error:": str(e)}
+            
+            return Success("Write successful")
+        except Exception as e:
+            return {"All server error:" : str(e)}
+    except Exception as e:
+        return {"error": str(e)}
 
+
+if __name__ == "__main__":
+    # Start the JSON-RPC server
+    serve(port=3999)
