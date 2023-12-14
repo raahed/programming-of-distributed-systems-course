3e3a45875a54bc913ba1f7b22dbab39a20dac08f
Andreas Bendheimer <bendheimeran84503@th-nuernberg.de>
2023-12-14 12:16:02 +0100

    Delete Auskommentierung

--- task-5/philosopher.py
+++ task-5/philosopher.py
@@ -12,14 +12,12 @@ fork_in_left_hand = False
 fork_in_rigt_hand = False
 right_fork_id = None; 
 
-
 def get_neighbours_id():
     global id
     if id + 1 > num_of_philosophers - 1:
         return 0
     else:
         return id + 1
-
 
 def thinking():
     global id
@@ -34,22 +32,6 @@ def cycle_through_state():
     status = status +1
     if (status > 2):
         status = 0
-
-
-# def check_forks():
-#     global id
-#     neigbour_id = get_neighbours_id()
-#     for philosopher in philosophers:
-#         try:
-#             if json_rpc_request("answer_fork_availability", {}, philosophers[neigbour_id]):
-#                 take_forks()
-#                 break
-#             else:      
-#                 time_interval = random.randint(1,10)
-#                 print("philosopher " + str(id) + " is waiting before sending another check_fork message for " + str(time_interval) + " seconds")
-#                 time.sleep(time_interval)
-#         except:
-
 
 def eating():
     global id
@@ -120,7 +102,6 @@ def take_forks():
                 being_hungry()
                 break
     else:
-        # error: I thought my fork was available but it is not
         print("I want to eat but my own fork is gone.")
         being_hungry()
 
@@ -136,19 +117,6 @@ def give_fork_away() -> rpc.Success:
     else:
         print("A fork was requested but I am using it")
         return rpc.Success(left_fork)
-
-    # if success == None: # I am the first one to answer
-    #     if left_fork == True:
-    #         left_fork = False
-    #         success = True
-    #         giversID = id
-    #         json_rpc_request("give_fork_away", {success, sendersID, giversID}, get_neighbours_id())
-    # else:
-    #     json_rpc_request("give_fork_away", {success, sendersID, giversID}, get_neighbours_id())
-
-    # if sendersID == id:
-    #      if success == True:
-    #           fork_in_rigt_hand = True
           
 @rpc.method
 def receive_fork_back(giversID: int) -> rpc.Success:
@@ -162,35 +130,6 @@ def receive_fork_back(giversID: int) -> rpc.Success:
     else:
         return rpc.Success(json_rpc_request("receive_fork_back", {"giversID":giversID}, philosophers[get_neighbours_id()]))
 
-
-# @rpc.method
-# def answer_fork_availability():
-#     return rpc.Success(left_fork)
-
-    # receiving your own message back
-    # if id == _id:
-    #     if (next_neighbour and last_neighbour):
-    #         # the forks are free!
-    #         take_forks()
-    #     else:
-    #         being_hungry()
-    # else:
-        # receiving a message from someone else
-        # if next_neighbour == None:
-        #     if status != 2:
-        #         next_neighbour = True
-        #     else:
-        #         next_neighbour = False
-
-        # # this part could be neglected as we implemented the forks 
-        # if status != 2:
-        #     last_neighbour = True
-        # else:
-        #     last_neighbour = False
-
-        # # pass on the message
-        # json_rpc_request("answer_fork_availability", {next_neighbour, last_neighbour, _id}, get_neighbours_id() )
-    
 def being_hungry():
     global id
     global left_fork
@@ -214,8 +153,6 @@ if __name__ == "__main__":
     
     server_thread = threading.Thread(target=start_server)
     server_thread.start()
-
-    #thinking()
 
     while(True):
         if (status == 0):
